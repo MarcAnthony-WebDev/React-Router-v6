4b5fe9e707086b76db170266d74abbf0207798f6
MarcAnthony-WebDev <marcanthony.webdev@gmail.com>
2023-10-08 14:43:44 -0400

    additions

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,36 @@
-import logo from "../assets/red30-tech-logo.png";
+import { Routes, Route, Link } from 'react-router-dom';
+import logo from '../assets/red30-tech-logo.png';
+
+import Home from './Home';
+import Categories from './Categories';
+import About from './About';
 
 function App() {
   return (
-    <div className="app">
-      <header className="container">
+    <div className='app'>
+      <header className='container'>
         <img
-          className="logo"
+          className='logo'
           src={logo}
-          alt="Red30 Tech logo"
-          title="Red30 Tech | Home"
+          alt='Red30 Tech logo'
+          title='Red30 Tech | Home'
         />
+
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/categories'>Categories</Link>
+          <Link to='/about'>About</Link>
+        </nav>
       </header>
 
-      <footer className="container">
-        &copy;2022 | <a href="https://red30tech.com/">Red30 Tech</a>
+      <Routes>
+        <Route path='/' element={<Home />} title='welcome to home 30 tech' />
+        <Route path='categories' element={<Categories />} />
+        <Route path='about' element={<About />} />
+      </Routes>
+
+      <footer className='container'>
+        &copy;2022 | <a href='https://red30tech.com/'>Red30 Tech</a>
       </footer>
     </div>
   );
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
+import { getCategories } from '../api';
+
 export default function Categories() {
+  const categories = getCategories();
+
   return (
-    <div className="container">
+    <div className='container'>
       <h1>Session Categories</h1>
+
+      <ul className='categories'>
+        {categories.map((cat) => (
+          <li key={cat.id}>{cat.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
